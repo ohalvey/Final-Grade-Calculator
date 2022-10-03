fa9ca42d3fd9c36072d958d3d15c6d7f9234d2f4
oliver halvey <oliverhalvey310@yahoo.com>
2022-10-03 08:07:19 -0700

    Initial Commit

---
+++ Final Grade Calculator/Final_Grade_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Final_Grade_CalculatorApp.swift
+//  Final Grade Calculator
+//
+//  Created by Oliver Halvey on 10/3/22.
+//
+
+import SwiftUI
+
+@main
+struct Final_Grade_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
